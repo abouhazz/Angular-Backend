ab43e41e41bb1375d8ae2afd3e9df2d71be3e6f0
abouhazz <ayman_elb112@hotmail.com>
2019-04-17 16:46:29 +0200

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Running on port 3000`);
+app.listen(PORT, () => {
+    console.log(`Running on port ${ PORT }`);
 });
