1fa17bd3e6c384097feb31665d20f97af079201e
abouhazz <ayman_elb112@hotmail.com>
2019-04-17 16:43:55 +0200

    asfafs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Running on port ${ PORT }`);
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Running on port 3000`);
 });
